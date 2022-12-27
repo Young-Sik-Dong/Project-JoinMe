661fcd3dbe5c05f8271b3f1087d0b57fbe5b0d95
DESKTOP-R0I5ASH\Dong's <ysdong1206@gmail.com>
2022-12-27 18:23:44 +0900

    클래스명 변경

--- src/main/java/himedia/joinme/repository/JoinmeRepository.java
+++ src/main/java/himedia/joinme/repository/JoinmeRepository.java
@@ -3,9 +3,7 @@ package himedia.joinme.repository;
 import java.util.List;
 import java.util.Optional;
 
-
-public interface PostRepository {
+public interface JoinmeRepository {
 	Object save(Object object);
-	Optional<Object> findByPostInfo(Long index, String PostNo);
 	List<Object> findAll();
 }
