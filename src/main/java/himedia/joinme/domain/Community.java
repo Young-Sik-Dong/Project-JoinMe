4780c8157b62ef9b4016251fd7fcd20911a9b5c7
DESKTOP-R0I5ASH\Dong's <ysdong1206@gmail.com>
2022-12-27 17:47:55 +0900

    코드 수정
    lombok 어노테이션 사용을 위한 코드 수정

--- src/main/java/himedia/joinme/domain/Community.java
+++ src/main/java/himedia/joinme/domain/Community.java
@@ -1,58 +1,25 @@
 package himedia.joinme.domain;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter @Setter
+@NoArgsConstructor
 public class Community {
 	private String postNo;
 	private String postTitle;
 	private String postWriter;
 	private String postDate;
+	@Id @GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long index;
 	private String category;
 	private String text;
 	private String joinLink;
-	
-	public String getPostNo() {
-		return postNo;
-	}
-	public void setPostNo(String postNo) {
-		this.postNo = postNo;
-	}
-	public String getPostTitle() {
-		return postTitle;
-	}
-	public void setPostTitle(String postTitle) {
-		this.postTitle = postTitle;
-	}
-	public String getPostWriter() {
-		return postWriter;
-	}
-	public void setPostWriter(String postWriter) {
-		this.postWriter = postWriter;
-	}
-	public String getPostDate() {
-		return postDate;
-	}
-	public void setPostDate(String postDate) {
-		this.postDate = postDate;
-	}
-	public Long getIndex() {
-		return index;
-	}
-	public String getCategory() {
-		return category;
-	}
-	public void setCategory(String category) {
-		this.category = category;
-	}
-	public String getText() {
-		return text;
-	}
-	public void setText(String text) {
-		this.text = text;
-	}
-	public String getJoinLink() {
-		return joinLink;
-	}
-	public void setJoinLink(String joinLink) {
-		this.joinLink = joinLink;
-	}
 }
--- src/main/java/himedia/joinme/domain/Contest.java
+++ src/main/java/himedia/joinme/domain/Contest.java
@@ -1,10 +1,23 @@
 package himedia.joinme.domain;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter @Setter
+@NoArgsConstructor
 public class Contest{
 	private String postNo;
 	private String postTitle;
 	private String postWriter;
 	private String postDate;
+	@Id @GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long index;
 	private String contestField;
 	private String companyName;
@@ -14,80 +27,4 @@ public class Contest{
 	private String startDate;
 	private String endDate;
 	private String contestLink;
-	
-	public String getPostNo() {
-		return postNo;
-	}
-	public void setPostNo(String postNo) {
-		this.postNo = postNo;
-	}
-	public String getPostTitle() {
-		return postTitle;
-	}
-	public void setPostTitle(String postTitle) {
-		this.postTitle = postTitle;
-	}
-	public String getPostWriter() {
-		return postWriter;
-	}
-	public void setPostWriter(String postWriter) {
-		this.postWriter = postWriter;
-	}
-	public String getPostDate() {
-		return postDate;
-	}
-	public void setPostDate(String postDate) {
-		this.postDate = postDate;
-	}
-	public Long getIndex() {
-		return index;
-	}
-	public String getContestField() {
-		return contestField;
-	}
-	public void setContestField(String contestField) {
-		this.contestField = contestField;
-	}
-	public String getCompanyName() {
-		return companyName;
-	}
-	public void setCompanyName(String companyName) {
-		this.companyName = companyName;
-	}
-	public String getTargetName() {
-		return targetName;
-	}
-	public void setTargetName(String targetName) {
-		this.targetName = targetName;
-	}
-	public String getHostName() {
-		return hostName;
-	}
-	public void setHostName(String hostName) {
-		this.hostName = hostName;
-	}
-	public String getReward() {
-		return reward;
-	}
-	public void setReward(String reward) {
-		this.reward = reward;
-	}
-	public String getStartDate() {
-		return startDate;
-	}
-	public void setStartDate(String startDate) {
-		this.startDate = startDate;
-	}
-	public String getEndDate() {
-		return endDate;
-	}
-	public void setEndDate(String endDate) {
-		this.endDate = endDate;
-	}
-	public String getContestLink() {
-		return contestLink;
-	}
-	public void setContestLink(String contestLink) {
-		this.contestLink = contestLink;
-	}
 }
