757a44d3687fac5b7f48c4b7415012e718aaac67
DESKTOP-R0I5ASH\Dong's <ysdong1206@gmail.com>
2022-12-29 17:49:56 +0900

    SQL 파일 생성
    DB 테스트를 위한 파일

---
+++ database/test_code.sql
@@ -0,0 +1,14 @@
+use joinme;
+
+insert into member values('id1','pw1','홍길동','홍',now());
+
+update member
+set member_password = 'pw2',
+	member_name = '홍길순',
+    nickname = '길',
+    modify_date = now()
+where member_id like 'id1';
+
+select * from member;
+
+# 1 row(s) affected, 1 warning(s): 1292 Incorrect date value: '2022-12-29 17:34:31' for column 'modify_date' at row 1 Rows matched: 1  Changed: 1  Warnings: 1
