9a117e5380c91f22014c8cc463d147e6a0436ecb
DESKTOP-R0I5ASH\Dong's <ysdong1206@gmail.com>
2023-01-10 12:40:16 +0900

    eclipse - DB 연동 sql 파일 수정
    데이터 수정

--- src/main/resources/db/realdb/data-mysql.sql
+++ src/main/resources/db/realdb/data-mysql.sql
@@ -3,28 +3,28 @@ insert into member(member_id, member_password, nickname)
 			('id2', 'pw2', 'hi2'),
 			('id3', 'pw3', 'hi3');
 
-insert into post(post_name, member_no, title, textbox) 
-	values	('CONTEST', 1, '공모전 새글1', '안녕하세요1'),
-			('CONTEST', 2, '공모전 새글2', '안녕하세요2'),
-			('CONTEST', 3, '공모전 새글3', '안녕하세요3'),
-			('JOIN',  1, '모집글 새글1', '안녕하세요1'),
-			('JOIN', 2, '모집글 새글2', '안녕하세요2'),
-			('JOIN', 3, '모집글 새글3', '안녕하세요3'),
-			('COMMUNITY', 1, '커뮤니티 새글1', '안녕하세요1'),
-			('COMMUNITY', 2, '커뮤니티 새글2', '안녕하세요2'),
-			('COMMUNITY', 3, '커뮤니티 새글3', '안녕하세요3');
+insert into post(post_name, member_no) 
+	values	('CONTEST', 1),
+			('CONTEST', 2),
+			('CONTEST', 3),
+			('JOIN',  1),
+			('JOIN', 2),
+			('JOIN', 3),
+			('COMMUNITY', 1),
+			('COMMUNITY', 2),
+			('COMMUNITY', 3);
 
 insert into contest 
-	values	(1, 1, 'a', 'hi', 'hi','hi', '1억', '2022-01-06', '2022-02-10', 'link1'),
-			(2, 2, 'a', 'hi', 'hi','hi', '1억', '2022-01-06', '2022-02-10', 'link2'),
-            (3, 3, 'a', 'hi', 'hi','hi', '1억', '2022-01-06', '2022-02-10', 'link3');
+	values	(1, 1, '공모전 새글1','a', 'hi', 'hi','hi', '1억', '2022-01-06', '2022-02-10', 'link1'),
+			(2, 2, '공모전 새글2', 'a', 'hi', 'hi','hi', '1억', '2022-01-06', '2022-02-10', 'link2'),
+            (3, 3, '공모전 새글3', 'a', 'hi', 'hi','hi', '1억', '2022-01-06', '2022-02-10', 'link3');
      
 insert into recruitment
-	values	(4, 1, 'SEOUL', 'link1'),
-			(5, 1, 'SEOUL', 'link1'),
-            (6, 1, 'SEOUL', 'link1');
+	values	(4, 1, '모집글 새글1', '안녕하세요1', 'SEOUL', 'link1'),
+			(5, 1, '모집글 새글2', '안녕하세요2', 'SEOUL', 'link1'),
+            (6, 1, '모집글 새글3', '안녕하세요3', 'SEOUL', 'link1');
             
 insert into community 
-	values	(7, 'STUDY'),
-			(8, 'STUDY'),
-            (9, 'STUDY'); 
+	values	(7, '커뮤니티 새글1', '안녕하세요1', 'STUDY'),
+			(8, '커뮤니티 새글2', '안녕하세요2', 'STUDY'),
+            (9, '커뮤니티 새글3', '안녕하세요3', 'STUDY'); 
