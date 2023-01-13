dac3226fb5b6da3cade5f584f6d5004da4c3afa0
DESKTOP-R0I5ASH\Dong's <ysdong1206@gmail.com>
2023-01-13 10:16:45 +0900

    [ joinme - DB ]
    내부 DB 코드 수정

--- database/test_code.sql
+++ database/test_code.sql
@@ -6,6 +6,8 @@ select * from member where member_no = 1;
 insert into member(member_id, member_password, nickname) 
 	values	('id4', 'pw4', 'hi4');
 
+show tables;
+
 select * from member;
 
 select * from post order by post_no desc;
@@ -16,6 +18,9 @@ member_password = "1357",
 nickname = "nick5",
 modify_date = current_date
 where member_id = 'id4';
+
+desc post;
+desc contest;
 
 delete from member
 where member_id like 'id4';
@@ -31,3 +36,23 @@ select * from community;
 select * from contest;
 select * from reply;
 select * from scarp;
+
+SELECT 
+	a.TABLE_NAME '테이블명',
+	b.ORDINAL_POSITION '순번', 
+	b.COLUMN_NAME '필드명', 
+	b.DATA_TYPE 'DATA TYPE', 
+	b.COLUMN_TYPE '데이터길이', 
+	b.COLUMN_KEY 'KEY', 
+	b.IS_NULLABLE 'NULL값여부', 
+	b.EXTRA '자동순번', 
+	b.COLUMN_DEFAULT '기본값', 
+	b.COLUMN_COMMENT '필드설명' 
+FROM information_schema.TABLES a 
+	JOIN information_schema.COLUMNS b 
+	ON a.TABLE_NAME = b.TABLE_NAME 
+	AND a.TABLE_SCHEMA = b.TABLE_SCHEMA 
+WHERE a.TABLE_SCHEMA = 'joinme' 
+-- AND a.TABLE_NAME = '테이블명'   // 특정 테이블만 조회
+ORDER BY a.TABLE_NAME, b.ORDINAL_POSITION;
+
